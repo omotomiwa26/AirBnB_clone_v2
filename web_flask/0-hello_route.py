bd207a8ef29281ffca5aa30bb7ebe28507879a61
omotomiwa26 <omotomiwa26@gmail.com>
2024-01-29 02:16:50 +0000

    display “Hello HBNB!”

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This Python script starts a
+Flask application
+"""
+
+
+from flask import Flask as f
+app = f(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    This flask route returns Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
