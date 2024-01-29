8b64777624119aaeb58f3816aec82774f45fc639
omotomiwa26 <omotomiwa26@gmail.com>
2024-01-29 13:26:39 +0000

    fixed pep8

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(
         String(128), nullable=False
-    ) 
+    )
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship(
             'City',
