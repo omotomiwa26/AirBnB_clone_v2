95045938c96ae77287e4bb6e2b5a8eec1331f2f3
omotomiwa26 <omotomiwa26@gmail.com>
2024-02-05 00:25:10 +0000

    script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers,

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
-This python Fabric script(based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy
+This python Fabric script(based on the file
+2-do_deploy_web_static.py) that creates
+and distributes an archive to your
+web servers, using the function deploy
 """
 
 from fabric.api import env, local, put, run
